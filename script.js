5640d2b7710eeac3e51eed7b3a56d113402500f7
Benjamin Mizrahi <benjajamin@gmail.com>
2015-09-09 19:49:31 +0300

    Added footers

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ $('document').ready(function(){
 	$('#page').hide();
 	$.getJSON("https://www.reddit.com/r/earthporn/top.json?sort=top&t=all&limit=100",function(json){
 		var rand=Math.floor(Math.random() * 100);
-		var url=json.data.children[rand].data.url;
+		var post=json.data.children[rand].data;
+		var url=post.url;
+		var author=post.author;
+		var title=post.title.toLowerCase();
+		var imageurl=post.permalink;
+		$('#imageurl').attr("href","http://www.reddit.com"+imageurl);
+		$('#picby').html("Photo by: u/"+author);
+		if(title.indexOf("[oc]")==-1){
+			$('#r').hide();
+		}
 		betterUrl=tryConvertUrl(url);
 		if(betterUrl!=''){
 			url=betterUrl;
@@ -10,7 +19,7 @@ $('document').ready(function(){
 		$('<img/>').attr('src', url).load(function() {
    			$(this).remove(); // prevent memory leaks
    			$('#loading').hide();
-   			$('#page').css('background-image', "url("+url+")");	
+   			$('#page').css('background-image', "url("+url+")");		
    			$('#page').fadeIn("slow");
 		});	
 	});
@@ -19,8 +28,10 @@ $('document').ready(function(){
 		var post=json.data.children[rand].data;
 		var quote=post.title;
 		var author=post.author;
+		var quoteurl=post.permalink;
 		$('#quote').html("\""+quote+"\"");
 		$('#author').html("u/"+author);
+		$('#quoteurl').attr("href","http://www.reddit.com"+quoteurl);
 	});
 
 });
