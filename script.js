f40578771b58cb6750ef8dc2675ce8c74b503bb9
Nick Reynolds <nreynolds@nreynolds-mbp1.local>
2015-09-10 19:48:03 -0400

    Restyle and center align
    Align page center, restyle attribution links to be links on the text themselves. Other random changes to clean up a bit.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $('document').ready(function(){	
-	$('#page').hide();
+	$('.content').hide();
+
 	$.getJSON("https://www.reddit.com/r/earthporn/top.json?sort=top&t=all&limit=100",function(json){
 		var rand=Math.floor(Math.random() * 100);
 		var post=json.data.children[rand].data;
 		var url=post.url;
 		var author=post.author;
 		var title=post.title.toLowerCase();
-		var imageurl=post.permalink;
-		$('#imageurl').attr("href","http://www.reddit.com"+imageurl);
-		$('#picby').html("Photo by: u/"+author);
+		var imageurl="http://www.reddit.com"+post.permalink;
+		$('#picby').html("<a href='"+imageurl+"' target='_blank'>photo by u/"+author+"</a>");
 		if(title.indexOf("[oc]")==-1){
 			$('#r').hide();
 		}
@@ -18,20 +18,20 @@ $('document').ready(function(){
 		}
 		$('<img/>').attr('src', url).load(function() {
    			$(this).remove(); // prevent memory leaks
-   			$('#loading').hide();
-   			$('#page').css('background-image', "url("+url+")");		
-   			$('#page').fadeIn("slow");
+   			$('#loading').fadeOut("fast");
+
+   			$('#image').css('background-image', "url("+url+")");		
+   			$('.content').fadeIn("slow");
 		});	
 	});
-	$.getJSON("https://www.reddit.com/r/showerthoughts/top.json?sort=top&t=all&limit=100",function(json){
+	$.getJSON("https://www.reddit.com/r/showerthoughts/top.json?sort=top&t=all&limit=100",function(json) {
 		var rand=Math.floor(Math.random() * 100);
 		var post=json.data.children[rand].data;
 		var quote=post.title;
 		var author=post.author;
-		var quoteurl=post.permalink;
+		var quoteurl="http://www.reddit.com"+post.permalink;
 		$('#quote').html("\""+quote+"\"");
-		$('#author').html("u/"+author);
-		$('#quoteurl').attr("href","http://www.reddit.com"+quoteurl);
+		$('#author').html(" - <a href='"+quoteurl+"' target='_blank'>u/"+author+"</a>");
 	});
 
 });
