a7ea488e8fb6c8b9714dcc9909d158f669ff5b7b
Nick Reynolds <nreynolds@nreynolds-mbp1.local>
2015-10-15 21:03:12 -0700

    Only show one image per day to improve performance when loading new tab

--- script.js
+++ script.js
@@ -1,40 +1,70 @@
-$('document').ready(function(){	
+var appstore = chrome.storage.sync;
+
+$('document').ready(function(){
 	$('.content').hide();
 
+    appstore.get(['date', 'shower', 'earth'], function(data) {
+    	var today = (new Date()).toDateString();
+
+    	if (isValidData(data) && data.date == today) {
+    		var earth = data.earth;
+    		setEarthPorn(earth.author, earth.postUrl, earth.imageUrl);
+
+    		var shower = data.shower;
+    		setShowerThought(shower.author, shower.quote, shower.quoteUrl);
+
+    	} else {
+    		appstore.set({date: today}, null);
+    		loadFromNetwork();
+    	}
+    });
+});
+
+var isValidData = function (data) {
+	return data != null && data.shower != null && data.earth != null && data.date != null;
+}
+
+var loadFromNetwork = function (data) {
 	$.getJSON("https://www.reddit.com/r/earthporn/top.json?sort=top&t=all&limit=100",function(json){
 		var rand=Math.floor(Math.random() * 100);
 		var post=json.data.children[rand].data;
 		var url=post.url;
 		var author=post.author;
 		var title=post.title.toLowerCase();
-		var imageurl="http://www.reddit.com"+post.permalink;
-		$('#picby').html("<a href='"+imageurl+"' target='_blank'>photo by u/"+author+"</a>");
-		if(title.indexOf("[oc]")==-1){
-			$('#r').hide();
-		}
-		betterUrl=tryConvertUrl(url);
-		if(betterUrl!=''){
-			url=betterUrl;
-		}
-		$('<img/>').attr('src', url).load(function() {
-   			$(this).remove(); // prevent memory leaks
-   			$('#loading').fadeOut("fast");
+		var postUrl="http://www.reddit.com"+post.permalink;
 
-   			$('#image').css('background-image', "url("+url+")");		
-   			$('.content').fadeIn("slow");
-		});	
+		var imageUrl=tryConvertUrl(url);
+
+		appstore.set({earth: {author: author, postUrl: postUrl, imageUrl: imageUrl}}, null);
+		setEarthPorn(author, postUrl, imageUrl);
 	});
 	$.getJSON("https://www.reddit.com/r/showerthoughts/top.json?sort=top&t=all&limit=100",function(json) {
 		var rand=Math.floor(Math.random() * 100);
 		var post=json.data.children[rand].data;
 		var quote=post.title;
 		var author=post.author;
-		var quoteurl="http://www.reddit.com"+post.permalink;
-		$('#quote').html("\""+quote+"\"");
-		$('#author').html(" - <a href='"+quoteurl+"' target='_blank'>u/"+author+"</a>");
-	});
+		var quoteUrl="http://www.reddit.com"+post.permalink;
 
-});
+		appstore.set({shower: {author: author, quote: quote, quoteUrl: quoteUrl}}, null);
+		setShowerThought(author, quote, quoteUrl);
+	});
+}
+
+var setEarthPorn = function (author, postUrl, imageUrl) {
+	$('#picby').html("<a href='"+postUrl+"' target='_blank'>photo by u/"+author+"</a>");
+	$('<img/>').attr('src', imageUrl).load(function() {
+		$(this).remove(); // prevent memory leaks
+		$('#loading').fadeOut("fast");
+
+		$('#image').css('background-image', "url("+imageUrl+")");
+		$('.content').fadeIn("slow");
+	});	
+}
+
+var setShowerThought = function (author, quote, quoteUrl) {
+	$('#quote').html("\""+quote+"\"");
+	$('#author').html(" - <a href='"+quoteUrl+"' target='_blank'>u/"+author+"</a>");
+}
 
 var tryConvertUrl = function (url) {
 	if (url.indexOf('imgur.com') > 0 || url.indexOf('/gallery/') > 0) {
@@ -49,5 +79,5 @@ var tryConvertUrl = function (url) {
 		}
 		return url.replace(/r\/[^ \/]+\/(\w+)/, '$1') + '.jpg';
 	}
-	return '';
+	return url;
 };
